76040345857777e80836fde37d025b1048664a7f
Klaas Schoute <klaas_schoute@hotmail.com>
2023-08-09 22:38:27 +0200

    Update code for Ruff

--- src/odp_gent/odp_gent.py
+++ src/odp_gent/odp_gent.py
@@ -5,7 +5,7 @@ import asyncio
 import socket
 from dataclasses import dataclass
 from importlib import metadata
-from typing import Any, cast
+from typing import TYPE_CHECKING, Any, cast
 
 import async_timeout
 from aiohttp import ClientError, ClientSession
@@ -14,6 +14,9 @@ from yarl import URL
 
 from .exceptions import ODPGentConnectionError, ODPGentError
 from .models import BlueBike, Garage, ParkAndRide, Partago
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
 
 
 @dataclass
@@ -185,7 +188,7 @@ class ODPGent:
         if self.session and self._close_session:
             await self.session.close()
 
-    async def __aenter__(self) -> ODPGent:
+    async def __aenter__(self) -> Self:
         """Async enter.
 
         Returns
@@ -194,7 +197,7 @@ class ODPGent:
         """
         return self
 
-    async def __aexit__(self, *_exc_info: Any) -> None:
+    async def __aexit__(self, *_exc_info: object) -> None:
         """Async exit.
 
         Args:
