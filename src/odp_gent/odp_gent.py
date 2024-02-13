f084e6dab7c1d62b08b8e304ca331507b989f45c
Klaas Schoute <klaas_schoute@hotmail.com>
2024-02-13 17:07:28 +0100

    Refactor pylint and ruff configurations

--- src/odp_gent/odp_gent.py
+++ src/odp_gent/odp_gent.py
@@ -77,7 +77,7 @@ class ODPGent:
                     ssl=True,
                 )
                 response.raise_for_status()
-        except asyncio.TimeoutError as exception:
+        except TimeoutError as exception:
             msg = "Timeout occurred while connecting to the Open Data Platform API."
             raise ODPGentConnectionError(msg) from exception
         except (ClientError, socket.gaierror) as exception:
